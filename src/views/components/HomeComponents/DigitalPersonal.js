6bac5441f5440d873c22a9466b7d58f2a17d7d70
shuvo-h <shuvoh38@gmail.com>
2022-11-29 16:43:56 +0600

    digital section added

---
+++ src/views/components/HomeComponents/DigitalPersonal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const digitalPersonData = {
+    title: "Digital, yet personal",
+    sub_title:"A mortgage experience made for you",
+    info:"We literally invented the Digital Mortgage. But we also offer one-on-one assistance at more than 500 branches across the U.S.​ Whether you prefer to stay at home or meet in person, we are here to tailor your mortgage experience to fit your needs.",
+    btn:"Find a branch near you"
+}
+
+const DigitalPersonal = () => {
+    return (
+        <section style={{display:"grid", gridTemplateColumns:"repeat(2,1fr)", gap:"2rem", borderTop:"1px solid lightgrey",borderBottom:"1px solid lightgrey", padding:"3rem 0"}} className="container">
+            <div>
+                <h2 style={{fontSize:"3rem", lineHeight:"3.5rem"}}>{digitalPersonData.title}</h2>
+                <h4 style={{fontSize:"2rem", lineHeight:"2.5rem", margin:0}}>{digitalPersonData.sub_title}</h4>
+                <p style={{fontSize:"1rem", lineHeight:"1.5rem"}}>{digitalPersonData.info}</p>
+                <div className='banner_buttons' style={{display:"block"}}>
+                    <button className='refinancing_btn'>{digitalPersonData.btn}</button>
+                </div>
+            </div>
+            <div>
+                <img style={{width:"100%", height:"100%"}} src={"/assets/images/digital.png"} alt="banner" />
+            </div>
+        </section>
+    );
+};
+
+export default DigitalPersonal;
--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Banner from '../components/HomeComponents/Banner';
 import Calculator from '../components/HomeComponents/Calculator';
+import DigitalPersonal from '../components/HomeComponents/DigitalPersonal';
 import PurchaseWork from '../components/HomeComponents/PurchaseWork';
 import NavBar from '../components/NavBar';
 
@@ -15,7 +16,8 @@ const Home = () => {
                 {/* <Banner></Banner> */}
                 {/* <p style={{padding:"20px 0",fontSize:"18px",backgroundColor:"#eff4f5", textAlign:"center"}}>Leave your slow, old-school bank in the dust. Join the future of mortgages. <span><a style={{fontWeight:"600", textDecoration:"none",color:"#6495ED"}} href="https://www.rate.com/redirect?dest=https://apply.guaranteedrate.com/apply/loan-purpose&icid=stickybar:getrates">Get personalized rates</a></span></p> */}
                 {/* <Calculator></Calculator> */}
-                <PurchaseWork></PurchaseWork>
+                {/* <PurchaseWork></PurchaseWork> */}
+                <DigitalPersonal></DigitalPersonal>
             </body>
             <footer>
                 <Footer></Footer>
