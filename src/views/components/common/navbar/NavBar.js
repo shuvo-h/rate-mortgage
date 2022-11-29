04b1a9e274393f3099e5875a6ea52fd58138f721
shuvo-h <shuvoh38@gmail.com>
2022-11-29 22:18:09 +0600

    fix nav hover issue

--- src/views/components/common/navbar/NavBar.js
+++ src/views/components/common/navbar/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./navbar.css";
 import { NavLink } from 'react-router-dom';
+import SearchIcon from '../../../../utils/icons/SearchIcon';
 
 
 const navMiddle = [
@@ -16,14 +17,16 @@ const navMiddle = [
                 name:"Mortgage Overview", url:"", 
                 sub_childs:[
                     {name:"Mortgage news", url:"",},
-                    {name:"Mortgage rate today", url:"",},
+                    {name:"Mortgage rates today", url:"",},
+                    {name:"Mortgage pre-approval", url:"",},
+                    {name:"Mortgage propcess", url:"",},
+                    {name:"Mortgage calculator", url:"",},
                 ]
             },
             {
                 name:"Refinance", url:"", 
                 sub_childs:[
                     {name:"Cash-out refinance", url:"",},
-                    {name:"Cash-out 22222222", url:"",},
                 ]
             },
         ]
@@ -34,11 +37,26 @@ const navMiddle = [
         url:"/",
         childs: [
             {
-                name:"Mortgage Overview", url:"", 
+                name:"Fixed Loans", url:"", 
                 sub_childs:[
-                    {name:"Mortgage news", url:"",},
+                    {name:"15-years fixed loans", url:"",},
+                    {name:"30-years fixed loans", url:"",},
                 ]
             },
+            {
+                name:"ARM Loans", url:"", 
+                sub_childs:[
+                    {name:"5-years ARM loans", url:"",},
+                    {name:"7-years ARM loans", url:"",},
+                    {name:"10-years ARM loans", url:"",},
+                ]
+            },
+            {name:"FHA loans", url:"", },
+            {name:"Interest only loans", url:"", },
+            {name:"VA loans", url:"", },
+            {name:"Jumbo loans", url:"", },
+            {name:"Personal loans", url:"", },
+            {name:"HELOC", url:"", },
         ]
     },
     {
@@ -48,9 +66,15 @@ const navMiddle = [
             {
                 name:"Closing cost calculator", url:"", 
                 sub_childs:[
-                    {name:"Mortgage news", url:"",},
+                    {name:"Closing cost calculator", url:"",},
+                    {name:"Refinance calculator", url:"",},
+                    {name:"Affordability calculator", url:"",},
+                    {name:"Extra payment calculator", url:"",},
+                    {name:"Points calculator", url:"",},
                 ]
             },
+            {name:"Home valuation tool", url:"",},
+            {name:"House market research", url:"",},
         ]
     },
     
@@ -74,18 +98,23 @@ const NavBar = () => {
                 {
                     navMiddle.map(navItem => <div className='nav_middle_items' key={navItem.name}>
                         <NavLink className={"link_style"} to={navItem.url}>{navItem.name}</NavLink>
-                        <div className='nav_childs'>
-                            {
-                                navItem.childs?.map(childNav => <div className={"nav_child"}  key={childNav.name}>
-                                    <NavLink className={"link_style nav_child_link"} to={childNav.url}>{childNav.name}</NavLink>
-                                    <div className='nav_subs'>
-                                        {
-                                            childNav.sub_childs?.map(subChild =><NavLink className={"link_style nav_child_link nav_sub"} to={subChild.url} key={subChild.name}>{subChild.name}</NavLink>)
-                                        }
-                                    </div>
-                                </div>)
-                            }
-                        </div>
+                        {
+                            navItem.childs && <div className={'nav_childs_wrapper'}>
+                                <div className='nav_childs'>
+                                    {
+                                        navItem.childs && navItem.childs?.map(childNav => <div className={"nav_child"}  key={childNav.name}>
+                                            <NavLink className={"link_style nav_child_link"} to={childNav.url}>{childNav.name}</NavLink>
+                                            <div className='nav_subs'>
+                                                {
+                                                    childNav.sub_childs && childNav.sub_childs?.map(subChild =><NavLink className={"link_style nav_child_link nav_sub"} to={subChild.url} key={subChild.name}>{subChild.name}</NavLink>)
+                                                }
+                                            </div>
+                                        </div>)
+                                    }
+                                </div>
+                            </div>
+                        }
+                        
                     </div>)
                 }
             </div>
@@ -94,10 +123,12 @@ const NavBar = () => {
                 {
                     navRight.map(navItem => <NavLink className={"link_style"} to={navItem.url} key={navItem.name}>{navItem.name}</NavLink>)
                 }
-                <NavLink className={"link_style"} to={""}>(Icon)</NavLink>
+                <NavLink className={"link_style search_icon"} to={""}><SearchIcon></SearchIcon></NavLink>
             </div>
         </nav>
     );
 };
 
+
 export default NavBar;
+
