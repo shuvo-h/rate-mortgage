12aa1e3f7b084bde4b92ac723bfd8c6b203282d2
shuvo-h <shuvoh38@gmail.com>
2022-11-30 17:57:14 +0600

    fix the css without round color icon

--- src/views/components/HomeComponents/Calculator.js
+++ src/views/components/HomeComponents/Calculator.js
@@ -45,98 +45,101 @@ const Calculator = () => {
 
     
     return (
-        <section style={{display:"grid", gridTemplateColumns:"repeat(2,1fr)", gap:"2rem",}} className="container">
-            <div>
-                <h2 style={{fontSize:"3rem", lineHeight:"3.5rem"}}>{calculatorData.title}</h2>
-                <p style={{fontSize:"1.5rem", lineHeight:"2rem"}}>{calculatorData.info}</p>
-                <div style={{display:"grid", gridTemplateColumns:"repeat(2,1fr)", gap:"2rem"}}>
-                    <div className='calc_Input_wrapper'>
-                        <p style={{margin:0}}>Home Price</p>
-                        <div>
-                            $
-                            <input className='calc_input' onChange={e=>setHomePrice(parseFloat(e.target.value))} type="number" value={homePrice} />
+        <section style={{display:"grid",}} className="container">
+            <h2 style={{fontSize:"3rem", lineHeight:"3.5rem"}}>{calculatorData.title}</h2>
+            <p style={{fontSize:"1.5rem", lineHeight:"2rem"}}>{calculatorData.info}</p>
+            <div style={{display:"grid", gridTemplateColumns:"repeat(2,1fr)", gap:"2rem"}}>
+                <div>
+                    <div style={{display:"grid", gridTemplateColumns:"repeat(2,1fr)", gap:"2rem"}}>
+                        <div className='calc_Input_wrapper'>
+                            <p style={{margin:0}}>Home Price</p>
+                            <div>
+                                $
+                                <input className='calc_input' onChange={e=>setHomePrice(parseFloat(e.target.value))} type="number" value={homePrice} />
+                            </div>
                         </div>
-                    </div>
-                    <div className='calc_Input_wrapper'>
-                        <p style={{margin:0}}>Down Payment %</p>
-                        <div> %
-                            <input className='calc_input' onChange={e=>setDownPayPercent(parseFloat(e.target.value))} type="number" value={downPayPercent} />
+                        <div className='calc_Input_wrapper'>
+                            <p style={{margin:0}}>Down Payment %</p>
+                            <div> %
+                                <input className='calc_input' onChange={e=>setDownPayPercent(parseFloat(e.target.value))} type="number" value={downPayPercent} />
+                            </div>
                         </div>
-                    </div>
-                    <div className='calc_Input_wrapper'>
-                        <p style={{margin:0}}>Down Payment $</p>
-                        <div>
-                            $
-                            <input className='calc_input' onChange={e=>setDownPay(parseFloat(e.target.value))} type="number" value={downPay} />
+                        <div className='calc_Input_wrapper'>
+                            <p style={{margin:0}}>Down Payment $</p>
+                            <div>
+                                $
+                                <input className='calc_input' onChange={e=>setDownPay(parseFloat(e.target.value))} type="number" value={downPay} />
+                            </div>
                         </div>
-                    </div>
-                    <div className='calc_Input_wrapper'>
-                        <p style={{margin:0}}>Loan term</p>
-                        <select  className='calc_input' style={{width:"100%",}} onChange={e=>setTermInMonth(parseFloat(e.target.value)*12)} name="" id="">
-                            {
-                                calculatorData.prices.loan_term.map(term => <option value={term} key={term}>{term}</option>)
-                            }
-                        </select>
-                    </div>
-                    <div className='calc_Input_wrapper'>
-                        <p style={{margin:0}}>Interest rate</p>
-                        <div>
-                            %
-                            <input className='calc_input' onChange={e=>setInterestRate(parseFloat(e.target.value))} type="number" value={interestRate} />
+                        <div className='calc_Input_wrapper'>
+                            <p style={{margin:0}}>Loan term</p>
+                            <select  className='calc_input' style={{width:"100%",}} onChange={e=>setTermInMonth(parseFloat(e.target.value)*12)} name="" id="">
+                                {
+                                    calculatorData.prices.loan_term.map(term => <option value={term} key={term}>{term}</option>)
+                                }
+                            </select>
+                        </div>
+                        <div className='calc_Input_wrapper'>
+                            <p style={{margin:0}}>Interest rate</p>
+                            <div>
+                                %
+                                <input className='calc_input' onChange={e=>setInterestRate(parseFloat(e.target.value))} type="number" value={interestRate} />
 
+                            </div>
+                        </div>
+                        <div style={{display:"flex", alignItems:"center", gap:"10px"}}>
+                            <input onChange={e=>setIsMilitaryVeteran(e.target.value)} type="checkbox" />
+                            <p style={{margin:0}}>Military Veteran</p>
+                        </div>
+                        <div className='banner_buttons' style={{display:"block"}}>
+                            <button>Apply now</button>
                         </div>
                     </div>
-                    <div style={{display:"flex", alignItems:"center", gap:"10px"}}>
-                        <input onChange={e=>setIsMilitaryVeteran(e.target.value)} type="checkbox" />
-                        <p style={{margin:0}}>Military Veteran</p>
-                    </div>
-                    <div className='banner_buttons' style={{display:"block"}}>
-                        <button>Apply now</button>
-                    </div>
-                </div>
-                <div style={{cursor:"pointer",padding:"5px", margin:"10px auto",boxShadow: isOpen? "0 0 2px 1px":""}} onClick={()=>setIsOpen(!isOpen)}>
-                    <p style={{color:"#6495ED", fontWeight:"600"}}>Legal Disclosures  (icon {isOpen ? "up" : "down"})</p>
-                    {
-                        isOpen && <div>
+                    <div style={{cursor:"pointer",padding:"5px", margin:"10px auto",boxShadow: isOpen? "0 0 2px 1px":""}} onClick={()=>setIsOpen(!isOpen)}>
+                        <p style={{color:"#6495ED", fontWeight:"600"}}>Legal Disclosures  (icon {isOpen ? "up" : "down"})</p>
                         {
-                            calculatorData.disclosures.map((para,idx) => <p key={idx}>{para}</p>)
+                            isOpen && <div>
+                            {
+                                calculatorData.disclosures.map((para,idx) => <p key={idx}>{para}</p>)
+                            }
+                        </div>
                         }
+                        
                     </div>
-                    }
-                    
                 </div>
-            </div>
-            <div style={{border:"1px solid skyblue", borderRadius:"8px"}}>
-                <div style={{textAlign:"center"}}>
-                    <p>Your monthly payment would be</p>
-                    <h2 style={{fontSize:"2.5rem", margin:0}}>$2500</h2>
-                </div>
-                <div style={{display:"flex", }}>
-                    <div style={{ width:"250px", height:"300px"}}>
-                        <GrapgMortgage graphData={dataForGraph} graphColors={COLORSForGraph}></GrapgMortgage>
+                <div style={{border:"1px solid skyblue", borderRadius:"8px", display:""}}>
+                    <div style={{textAlign:"center"}}>
+                        <p>Your monthly payment would be</p>
+                        <h2 style={{fontSize:"2.5rem", margin:0}}>$2500</h2>
                     </div>
-                    <div>
-                        <h5 style={{textAlign:"center"}}>Breakdown</h5>
-                        <div style={{width:"fit-content"}}>
-                            <div style={{display:"flex", justifyContent:"space-between", alignItems:"center",gap:"2rem"}}>
-                                <p style={{margin: "8px"}}><span style={{}}></span> Principal and interest</p>
-                                <h5 style={{margin:"8px"}}>$3654</h5>
-                            </div>
-                            <div style={{display:"flex", justifyContent:"space-between", alignItems:"center",gap:"2rem"}}>
-                                <p style={{margin: "8px"}}>PMI</p>
-                                <h5 style={{margin: "8px"}}>$223</h5>
-                            </div>
-                            <div style={{display:"flex", justifyContent:"space-between", alignItems:"center",gap:"2rem"}}>
-                                <p style={{margin:"8px"}}>Property taxes</p>
-                                <h5 style={{margin:"8px"}}>$17</h5>
-                            </div>
-                            <div style={{display:"flex", justifyContent:"space-between", alignItems:"center",gap:"2rem"}}>
-                                <p style={{margin:"8px"}}>Insurance</p>
-                                <h5 style={{margin: "8px"}}>$45</h5>
-                            </div>
-                            <div style={{display:"flex", justifyContent:"space-between", alignItems:"center",gap:"2rem"}}>
-                                <p style={{margin:"8px"}}>HOA fees</p>
-                                <h5 style={{margin:"8px"}}>$0</h5>
+                    <div style={{display:"flex", justifyContent:"center"}}>
+                        <div style={{ width:"250px", height:"300px"}}>
+                            <GrapgMortgage graphData={dataForGraph} graphColors={COLORSForGraph}></GrapgMortgage>
+                        </div>
+                        <div>
+                            <h5 style={{textAlign:"center"}}>Breakdown</h5>
+                            <div style={{width:"fit-content"}}>
+                                <div style={{display:"flex", justifyContent:"space-between", alignItems:"center",gap:"2rem"}}>
+                                    <span style={{width:"5px", height:"5px", background: "red"}}></span>
+                                    <p style={{margin: "8px", marginLeft:0}} data-amount="10">Principal and interest</p>
+                                    <h5 style={{margin:"8px"}}>$3654</h5>
+                                </div>
+                                <div style={{display:"flex", justifyContent:"space-between", alignItems:"center",gap:"2rem"}}>
+                                    <p style={{margin: "8px"}}>PMI</p>
+                                    <h5 style={{margin: "8px"}}>$223</h5>
+                                </div>
+                                <div style={{display:"flex", justifyContent:"space-between", alignItems:"center",gap:"2rem"}}>
+                                    <p style={{margin:"8px"}}>Property taxes</p>
+                                    <h5 style={{margin:"8px"}}>$17</h5>
+                                </div>
+                                <div style={{display:"flex", justifyContent:"space-between", alignItems:"center",gap:"2rem"}}>
+                                    <p style={{margin:"8px"}}>Insurance</p>
+                                    <h5 style={{margin: "8px"}}>$45</h5>
+                                </div>
+                                <div style={{display:"flex", justifyContent:"space-between", alignItems:"center",gap:"2rem"}}>
+                                    <p style={{margin:"8px"}}>HOA fees</p>
+                                    <h5 style={{margin:"8px"}}>$0</h5>
+                                </div>
                             </div>
                         </div>
                     </div>
