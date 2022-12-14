30ed22a0b84139a6bd6476a1728f72559a24a5f2
shuvo-h <shuvoh38@gmail.com>
2022-12-14 19:48:28 +0600

    added article data fetching

--- src/pages/resources/mortgage-news.js
+++ src/pages/resources/mortgage-news.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Layout from '../../components/Layout/layout';
-import MortgageNewsFeature from '../../components/resourcesCmp/MortgageNews/MortgageNews';
+// import React from 'react';
+// import Layout from '../../components/Layout/layout';
+// import MortgageNewsFeature from '../../components/resourcesCmp/MortgageNews/MortgageNews';
 
-const MortgageNews = () => {
-    return (
-        <Layout className='container'>
-            <MortgageNewsFeature></MortgageNewsFeature>
-        </Layout>
-    );
-};
+// const MortgageNews = () => {
+//     return (
+//         <Layout className='container'>
+//             <MortgageNewsFeature></MortgageNewsFeature>
+//         </Layout>
+//     );
+// };
 
-export default MortgageNews;
+// export default MortgageNews;
