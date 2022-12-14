d4cf565b98bd098487b88b463c9da1eae498d13c
Agwara <nnaemekaagwara@gmail.com>
2022-12-14 05:52:25 +0100

    styled 15year fixed page

--- src/components/MortgageRates/Assumption/15_year_fixedConf/Advantage15Year.js
+++ src/components/MortgageRates/Assumption/15_year_fixedConf/Advantage15Year.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
+import CustomAccordion from './CustomAccordion';
+
 const Advantage15Year = ({adDis_vantage, idString}) => {
     return (
         <section id={idString}>
             <h1>{adDis_vantage.title}</h1>
             <p>{adDis_vantage.introduction}</p>
-            <div>
+            <div >
                 {
                     adDis_vantage.terms.map((term,idx) =><div key={idx}>
-                        <h6>{term.title}</h6>
+                        {/* <h6>{term.title}</h6>
                         {
                             term.answer.map((ans,index) =><p key={index+500}>{ans}</p>)
-                        }
+                        } */}
+
+                        <CustomAccordion 
+                            title={term.title}
+                            accordionIndex={idx}
+                            content={term.answer.map((ans,index) =><p key={index+500}>{ans}</p>)}
+                        />
+                        
                     </div>)
                 }
             </div>
-            <div></div>
+            <div style={{height: "30px"}}></div>
         </section>
 
     );
